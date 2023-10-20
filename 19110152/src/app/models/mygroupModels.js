67765aaa1aa8ff52a8b932f5abafe1963b678781
kien1302 <huynhgiakien2001@gmail.com>
2023-10-20 23:04:16 +0700

    Week6

--- 19110152/src/app/models/mygroupModels.js
+++
@@ -1,5 +0,0 @@
-const mygroup = [
-    {id: "19110152", name:'Huynh Gia Kien'},
-    {id: "19110128", name:'Nguyen Quoc Hoang'},
-]
-module.exports = { mygroup }
--- 19110152/src/index.js
+++ 19110152/src/index.js
@@ -1,15 +1,19 @@
-const express = require('express')
-const app = express()
-const port = 5000
-const route = require('./routes');
-const bodyParser = require('body-parser');
+import React from 'react';
+import ReactDOM from 'react-dom/client';
+import {BrowserRouter} from 'react-router-dom'
+import App from './App';
+import reportWebVitals from './reportWebVitals';
 
-// app.get('/', (req, res) => {
-//   res.send('Hello World!')
-// })
-app.use(express.json())
-app.use(bodyParser.json());
-route(app);
-app.listen(port, () => {
-  console.log(`Example app listening on port ${port}`)
-})
+const root = ReactDOM.createRoot(document.getElementById('root'));
+root.render(
+  <React.StrictMode>
+    <BrowserRouter>
+      <App />
+    </BrowserRouter>
+  </React.StrictMode>
+);
+
+// If you want to start measuring performance in your app, pass a function
+// to log results (for example: reportWebVitals(console.log))
+// or send to an analytics endpoint. Learn more: https://bit.ly/CRA-vitals
+reportWebVitals();
